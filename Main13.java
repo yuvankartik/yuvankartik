b8547acfe76c5ef9827b158bb0cae15d265766d3
yuvankartik <103134867+yuvankartik@users.noreply.github.com>
2023-12-22 17:05:47 +0530

    Add files via upload

---
+++ Main13.java
@@ -0,0 +1,8 @@
+public class Main{
+    static int addition(int x , int y){
+        return x +y;
+    }
+    public static void main(String [] args){
+        System.out.println(addition(5,6));
+    }
+}
