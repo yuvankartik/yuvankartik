e78e41ec6d8661d3623be35393c18da4d5718ffd
yuvankartik <103134867+yuvankartik@users.noreply.github.com>
2023-12-22 12:33:59 +0530

    Multidimensional arrays

---
+++ Main7.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main (String[]args){
+        int[][] chooseNumbers = {{1,2,3,4,5,6} , {6,7,8,9,0}};
+        System.out.println(chooseNumbers[0][4]);
+    }
+}
