0e7461eca90ead48b603de85cd109976204df961
yuvankartik <103134867+yuvankartik@users.noreply.github.com>
2024-01-01 19:44:41 +0530

    Primitive types

---
+++ PrimitiveTypes.java
@@ -0,0 +1,9 @@
+public class PrimitiveTypes{
+public static void main(String[]args){
+    byte age = 14;
+    double height = 163.65;
+    char letter = 'Y';
+    boolean eligible = true;
+    System.out.println(age);
+}
+}
